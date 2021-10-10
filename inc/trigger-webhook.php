7b6cc793d5d178d345e2ab510d1e090adc3b62a9
Samuel Arendt <chryso.ad@gmail.com>
2021-10-10 19:12:55 -0300

    fix: Changes the check comparing the old status with the new one, and if both are public it starts deploying.

--- inc/trigger-webhook.php
+++ inc/trigger-webhook.php
@@ -3,32 +3,25 @@
 /**
  * Trigger the Webhook each time a Post or Page is updated
  */
-function wp_headless_trigger_trigger_webhook_on_save_post($post_id, $post)
+function wp_headless_trigger_trigger_webhook_on_save_post($new_status, $old_status, $post)
 {
-    if ($post->post_status !== 'publish' && $post->post_status !== 'trash') {
-        return;
-    }
-    // Ignore post revision
-    if (wp_is_post_revision($post_id)) {
-        return;
-    }
-    $wp_headless_trigger_settings = get_option('wp_headless_trigger_settings');
-    if (
-        !array_key_exists('wp_headless_trigger_webhook_url', $wp_headless_trigger_settings) ||
-        empty($wp_headless_trigger_settings['wp_headless_trigger_webhook_url'])
-    ) {
-        return false;
-    }
-
-    $webhook_url = esc_url($wp_headless_trigger_settings['wp_headless_trigger_webhook_url']);
-
-    $response = wp_remote_post($webhook_url);
-
-    if (is_wp_error($response)) {
-        $error_message = $response->get_error_message();
-        error_log("WP Headless Trigger: " . $error_message, 0);
-    } else {
-        update_option('wp_headless_trigger_last_trigger', time());
+    if ($new_status === 'publish' || $old_status === 'publish') {
+        $wp_headless_trigger_settings = get_option('wp_headless_trigger_settings');
+        if (
+            !array_key_exists('wp_headless_trigger_webhook_url', $wp_headless_trigger_settings) ||
+            empty($wp_headless_trigger_settings['wp_headless_trigger_webhook_url'])
+        ) {
+            return false;
+        }
+        pr('publicar', 1);
+        $webhook_url = esc_url($wp_headless_trigger_settings['wp_headless_trigger_webhook_url']);
+        $response = wp_remote_post($webhook_url);
+        if (is_wp_error($response)) {
+            $error_message = $response->get_error_message();
+            error_log("WP Headless Trigger: " . $error_message, 0);
+        } else {
+            update_option('wp_headless_trigger_last_trigger', time());
+        }
     }
 }
-add_action('save_post', 'wp_headless_trigger_trigger_webhook_on_save_post', 20, 2);
+add_action('transition_post_status', 'wp_headless_trigger_trigger_webhook_on_save_post', 10, 3);
